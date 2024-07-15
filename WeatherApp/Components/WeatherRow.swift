7fcce4cb566d0618a07a808735ab58826d4d5da7
OzgunEfe <ozgunefe.dev@gmail.com>
2024-07-15 23:15:46 +0100

    latest version. Some bugs fix and WeatherView finished.

---
+++ WeatherApp/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WeatherApp
+//
+//  Created by Ozgun Efe on 15/07/2024.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}

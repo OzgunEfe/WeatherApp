cce5c4af092f76b9a41eb2a0d7ca8fd8c85a8549
OzgunEfe <ozgunefe.dev@gmail.com>
2024-07-15 20:32:33 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ozgun Efe on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

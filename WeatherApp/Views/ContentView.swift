77c069b3823c9ee2a563f3b4398dd4744dfff0cd
OzgunEfe <ozgunefe.dev@gmail.com>
2024-07-15 22:47:32 +0100

    Created WeatherView and some UI

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             
             if let location = locationManager.location {
                 if let weather = weather {
-                    Text("Weather Data fecthed!")
+                    WeatherView(weather: weather)
                 } else {
                     LoadingView()
                         .task {
--- WeatherApp/Views/WeatherView.swift
+++ WeatherApp/Views/WeatherView.swift
@@ -8,11 +8,82 @@
 import SwiftUI
 
 struct WeatherView: View {
+    var weather: ResponseBody
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack{
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold()
+                        .font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack{
+                    HStack{
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDoube() + "°")
+                            .font(.system(size: 90))
+                            . fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity)
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack{
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.65, saturation: 0.78, brightness: 0.355))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.65, saturation: 0.78, brightness: 0.355))
+        .preferredColorScheme(.dark)
     }
 }
 
 #Preview {
-    WeatherView()
+    WeatherView(weather: previewWeather)
 }
